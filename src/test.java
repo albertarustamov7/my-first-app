b4af15491fd49c8d6eba3852daafa5019995303b
ВАСЯ <bmeliksetov@gmail.com>
2025-08-23 18:48:03 +0300

    Программа на ввод пароля и логина

---
+++ src/test.java
@@ -0,0 +1,27 @@
+
+// Программа на ввод пароль и логина
+import java.util.Scanner;
+public class test {
+    public static void main(String[] args) {
+        // Условные конструкции if-else
+        Scanner scanner = new Scanner(System.in);
+        //Блок орентированный на ввод Логина
+        System.out.println("Здравствуйте! Введите роль");
+        String role = scanner.nextLine();
+        if (role.equals("Admin")) {
+            // Блок орентированый на ввод пароля
+            System.out.println("Введите пароль");
+            int pass = scanner.nextInt();
+            if (pass == 123)
+                System.out.println("Добро пожаловать");
+            /* В случае если пользователь не
+            опознан, его просят ввести имя
+            после чего завершается программа
+             */
+            }else {
+                System.out.println("Слыш Пидор, ты кто такой");
+                String name = scanner.nextLine();
+                System.out.println("ну и пошел на хуй " + name);
+        }
+    }
+}

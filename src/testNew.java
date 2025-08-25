638098fd7d77f17028ae8e8b967f1d3797969fc3
ВАСЯ <bmeliksetov@gmail.com>
2025-08-25 10:42:52 +0300

    Добавил комментарий

--- src/testNew.java
+++ src/testNew.java
@@ -9,6 +9,19 @@ public class testNew {
         String char1 = scanner.next();
         System.out.println("Введите число");
         double number2 = scanner.nextDouble();
+/*
+как работает switch.
+switch  работает как конкретное заданное условие. Нельзя   сравнивать разные типы данных.
+switch работает по принципу: если условие 1 , то выводим  2
+если условие 3 то выводим 8
+Например:
+double result =0;  переменная вводится для приведения к  сумме ранее введеных переменных
+        switch (char1){ переменая в switch  нужна для того чтобы выполнить условие для расчета
+            case"+": result= number1+number2; Если пользователь в char1 записывает "+" то будет выражение case"+"
+                break; Блок кода на этом после расчета останавливается
+            case"-": result= number1-number2; Если пользователь в char1 записывает "-" то будет выражение case"-"
+                break;
+ */
         double result =0;
         switch (char1){
             case"+": result= number1+number2;

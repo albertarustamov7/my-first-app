17aeacec0b20c9046600a7174aae0396888aa2e9
ВАСЯ <bmeliksetov@gmail.com>
2025-09-23 13:37:21 +0300

    Работа с методами

--- src/Sum.java
+++ src/Sum.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class Sum {
+    // метод на вывод суммы чисел
+    static double sumNumber ( int a,  int b ){
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter two  numbers");
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        double result = sumNumber(num1,num2);
+        System.out.println( "Сумма" + result);
+
+        scanner.close();
+    }
 }

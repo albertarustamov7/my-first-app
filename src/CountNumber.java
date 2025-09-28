21a5e0ae3cf9b97652f0bfa4a1dafef18934b9b8
ВАСЯ <bmeliksetov@gmail.com>
2025-09-28 21:19:34 +0300

    Метод на подсчет и вывод четных чисел

--- src/CountNumber.java
+++ src/CountNumber.java
@@ -1,27 +1,49 @@
 import java.util.Scanner;
 
 public class CountNumber {
-    // Создание массива для метода и подсчета четных числе в это массиве
-    static double enterEvenNumbers(){// метод созданный
-        Scanner scanner = new Scanner(System.in);
-        int [] num = new int[10]; // СОЗДАННЫЙ МАССИВ ДЛЯ ЗАПИСИ ЧИСЕЛ
-        for (int i=0;i<num.length;i++){  // ЦИКЛ ДЛЯ ЗАПИСИ  В МАССИВ
-            System.out.println("Сумма"+(i+1)+':'); // ВЫВОД МАССИВА
-            num[i]=scanner.nextByte();// ЗАПИСЬ В МААССИВ
-        }
-        return num.length; // ВОЗВРАЩАЕМ КОЛЛИЧЕСВО СИМВОЛОВ
+
+    public static void welcome() {
+        System.out.println("Здравствуйте!");
     }
-    static double countNumbers(int eventNumber){//  метод для определения четности
-        int count = 0;
-        for ()
-        if (eventNumber % 2 == 0) {
-            count++;
+
+    // Метод для подсчёта СУММЫ чётных чисел
+    static int sumEvenNumbers(int[] numbers) {
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 == 0) {
+                sum += numbers[i];
+            }
         }
-        return ;
+        return sum;
     }
+
+    // Метод для ввода массива с клавиатуры
+    static int[] inputArray(Scanner scanner, int size) {
+        int[] arr = new int[size];
+        System.out.println("Введите " + size + " целых чисел:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        return arr;
+    }
+
+    // Основной метод вывода
+    static void outputResult(int[] numbers) {
+        welcome();
+        int sum = sumEvenNumbers(numbers);
+        System.out.println("Сумма чётных чисел: " + sum);
+    }
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        final int SIZE = 20;
 
-        System.out.println(enterEvenNumbers());
+        // Вводим 20 целых чисел
+        int[] massivNum = inputArray(scanner, SIZE);
 
+        // Выводим результат
+        outputResult(massivNum);
+
+        scanner.close();
     }
 }
